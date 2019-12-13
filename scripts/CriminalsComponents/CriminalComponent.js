3509ecdbe3c2700c44d246c01442c5de3bf33886
onteriowright <onteriowright@yahoo.com>
2019-12-13 11:41:56 -0600

    added css classes

--- scripts/CriminalsComponents/CriminalComponent.js
+++ scripts/CriminalsComponents/CriminalComponent.js
@@ -2,18 +2,18 @@ const CriminalComponent = criminal => {
   return `
     <div class="criminal-template">
       <div class="criminals criminal-heading">
-        <h2>${criminal.name}</h2></div><hr>
-        <div class="criminals"><strong>Age:</strong> ${criminal.age}</div><hr>
+        <h2>${criminal.name}</h2></div>
+        <div class="criminals"><strong>Age:</strong> ${criminal.age}</div>
         <div class="criminals"><strong>Conviction:</strong> ${
           criminal.conviction
-        }</div><hr>
+        }</div>
         <div class="criminals"><strong>Start:</strong> ${new Date(
           criminal.incarceration.start
-        ).toLocaleString("en-us")}</div><hr>
+        ).toLocaleString("en-us")}</div>
         <div class="criminals"><strong>End:</strong> ${new Date(
           criminal.incarceration.end
-        ).toLocaleString("en-us")}</div><hr>
-        <button id="associates--${criminal.id}">Associate Alibis</button>
+        ).toLocaleString("en-us")}</div>
+        <button id="associates--${criminal.id}">Show Associate Alibis</button>
     </div>
   `;
 };
