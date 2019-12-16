38dd0f502cf896ac2279d27d739b53d9c7110286
onteriowright <onteriowright@yahoo.com>
2019-12-15 19:08:22 -0600

    added dialog boxes for associates alibis

--- scripts/CriminalsComponents/CriminalComponent.js
+++ scripts/CriminalsComponents/CriminalComponent.js
@@ -14,6 +14,21 @@ const CriminalComponent = criminal => {
           criminal.incarceration.end
         ).toLocaleString("en-us")}</div>
         <button id="associates--${criminal.id}">Show Associate Alibis</button>
+
+        <dialog>
+          <div class="associates">
+            ${criminal.known_associates
+              .map(
+                associate =>
+                  `<div class="associates-card">
+                    <span class="dialog-titles">Associates Name:</span> ${associate.name}<br>
+                    <span class="dialog-titles">Alibi:</span> ${associate.alibi}
+                  </div><br>`
+              )
+              .join(" ")}
+          </div>
+          <button id="closeBtn">Close Dialog</button>
+        </dialog>
     </div>
   `;
 };
--- scripts/DialogComponent/Dialog.js
+++ scripts/DialogComponent/Dialog.js
@@ -4,34 +4,54 @@ const eventHub = document.querySelector("#mainContainer");
 const alibiTarget = document.querySelector("#alibiContainer");
 
 const DialogComponent = () => {
-  eventHub.addEventListener("associateBtnWasClicked", event => {
-    const criminals = UseCriminals();
+  const allCloseBtns = document.querySelectorAll("#closeBtn");
 
-    const foundCriminal = criminals.find(
-      singleCriminal => singleCriminal.id === parseInt(event.detail.criminalId)
-    );
-    const alibisHTML = foundCriminal.known_associates
-      .map(singleAssociate => {
-        return `
-          <div class="associates">
-            <strong>${singleAssociate.name}: </strong>${singleAssociate.alibi}  
-          </div>
-        `;
-      })
-      .join("");
-
-    document.querySelector(".alibi-text").innerHTML = alibisHTML;
-    document.querySelector(".alibis").showModal();
+  allCloseBtns.forEach(btn => {
+    btn.addEventListener("click", e => {
+      const dialogElement = e.target.parentNode;
+      dialogElement.close();
+    });
   });
 
-  const renderData = () => {
-    alibiTarget.innerHTML = `
-      <dialog class="alibis">
-        <div class="alibi-text"></div>
-        <button id="closeDialog" class="dialog-close-btn">Close Dialog</button>
-      </dialog>  
-    `;
-  };
-  renderData();
+  const allShowAssociatesBtns = document.querySelectorAll(
+    "button[id^='associates--']"
+  );
+
+  allShowAssociatesBtns.forEach(btn => {
+    btn.addEventListener("click", e => {
+      const dialogElement = document.querySelector(`#${e.target.id} + dialog`);
+      dialogElement.showModal();
+    });
+  });
+
+  //   eventHub.addEventListener("associateBtnWasClicked", event => {
+  //     const criminals = UseCriminals();
+
+  //     const foundCriminal = criminals.find(
+  //       singleCriminal => singleCriminal.id === parseInt(event.detail.criminalId)
+  //     );
+  //     const alibisHTML = foundCriminal.known_associates
+  //       .map(singleAssociate => {
+  //         return `
+  //           <div class="associates">
+  //             <strong>${singleAssociate.name}: </strong>${singleAssociate.alibi}
+  //           </div>
+  //         `;
+  //       })
+  //       .join("");
+
+  //     document.querySelector(".alibi-text").innerHTML = alibisHTML;
+  //     document.querySelector(".alibis").showModal();
+  //   });
+
+  //   const renderData = () => {
+  //     alibiTarget.innerHTML = `
+  //       <dialog class="alibis">
+  //         <div class="alibi-text"></div>
+  //         <button id="closeDialog" class="dialog-close-btn">Close Dialog</button>
+  //       </dialog>
+  //     `;
+  //   };
+  //   renderData();
 };
 export default DialogComponent;
